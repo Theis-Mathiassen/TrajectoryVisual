7c26914dbe1b481efeef5c8fdd92d9dc4d1b34b0
Daniel Hilo Hansen <55060805+KYNIKER@users.noreply.github.com>
2025-02-26 11:11:47 +0100

    Feat: Added template functions for generating params for the different query types.

--- src/ParamUtil.py
+++ src/ParamUtil.py
@@ -8,9 +8,10 @@ from rtree import index
 # Util to init params for different query types.
 class ParamUtil:
     # init the params to be the bounding box of the Rtree and fix some delta
-    def __init__(self, rtree: index.Index, trajectories: list(Trajectory), delta = 0):
+    def __init__(self, rtree: index.Index, trajectories: list[Trajectory], delta = 0, k = 3, eps = 1, linesMin = 3):
         boundingBox = rtree.bounds()
         
+        # Establish mbr for the whole Rtree
         self.xMin = boundingBox[0]
         self.xMax = boundingBox[1]
         self.yMin = boundingBox[2]
@@ -18,12 +19,64 @@ class ParamUtil:
         self.tMin = boundingBox[4]
         self.tMax = boundingBox[5]
         
+        # List of trajectories and their nodes. Note that every element of trajectory.nodes may not be in a simplified Rtree
         self.trajectories = trajectories
+        
+        # Delta for similarity queries
         self.delta = delta
+        # k for knn queries
+        self.k = k
+        # eps and minimum number of lines in a cluster for cluster queries
+        self.eps = eps
+        self.linesMin = linesMin
+        
         self.origin: Trajectory = None
         
     
-    def RangeParams(self, rtree: index.Index):
-        randomTrajectory = random.choice(self.trajectories)
-        centerNode: Node = randomTrajectory.nodes[len(randomTrajectory.nodes) // 2]
-        
+    
+    # The following presents different functions to generate params (dictionary) for the different types of queries. 
+    # Note that some values are None and needs changing depending on how we choose queries
+    def rangeParams(self, rtree: index.Index):
+        randomTrajectory: Trajectory = random.choice(self.trajectories)
+        centerNode: Node = randomTrajectory.nodes[len(randomTrajectory.nodes) // 2] # May be deleted depending on choice of range query generation
+        tMin = randomTrajectory.nodes[0].t 
+        tMax = randomTrajectory.nodes[-1].t
+        xMin = None
+        xMax = None
+        yMin = None
+        yMax = None
+        return dict(t1 = tMin, t2= tMax, x1 = xMin, x2 = xMax, y1 = yMin, y2 = yMax, delta = self.delta, k = self.k, origin = randomTrajectory, eps = self.eps, linesMin = self.linesMin)
+    
+    def similarityParams(self, rtree: index.Index):
+        randomTrajectory: Trajectory = random.choice(self.trajectories)
+        tMin = randomTrajectory.nodes[0].t 
+        tMax = randomTrajectory.nodes[-1].t
+        xMin = self.xMin
+        xMax = self.xMax
+        yMin = self.yMin
+        yMax = self.yMax
+        delta = None
+        return dict(t1 = tMin, t2= tMax, x1 = xMin, x2 = xMax, y1 = yMin, y2 = yMax, delta = delta, k = self.k, origin = randomTrajectory, eps = self.eps, linesMin = self.linesMin)
+    
+    def knnParams(self, rtree: index.Index):
+        randomTrajectory: Trajectory = random.choice(self.trajectories)
+        tMin = self.tMin 
+        tMax = self.tMax
+        xMin = self.xMin
+        xMax = self.xMax
+        yMin = self.yMin
+        yMax = self.yMax
+        k = None
+        return dict(t1 = tMin, t2= tMax, x1 = xMin, x2 = xMax, y1 = yMin, y2 = yMax, delta = self.delta, k = k, origin = randomTrajectory, eps = self.eps, linesMin = self.linesMin)
+    
+    def clusterParams(self, rtree: index.Index):
+        tMin = self.tMin 
+        tMax = self.tMax
+        xMin = self.xMin
+        xMax = self.xMax
+        yMin = self.yMin
+        yMax = self.yMax
+        k = self.k
+        eps = None
+        linesMin = None
+        return dict(t1 = tMin, t2= tMax, x1 = xMin, x2 = xMax, y1 = yMin, y2 = yMax, delta = self.delta, k = self.k, origin = self.origin, eps = eps, linesMin = linesMin)

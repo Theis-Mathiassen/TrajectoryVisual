5e1bb3b1c8168d6b5429670bff62b948c27ca3c7
HjalteJohnsen <101054167+HjalteJohnsen@users.noreply.github.com>
2025-03-10 11:54:11 +0100

    test: Added test cases for RangeQuery plotting and SimilarityQuery plotting. Also updated testMain.

--- test/testPlot.py
+++ test/testPlot.py
@@ -18,6 +18,8 @@ sys.path.append(full_path_src)
 sys.path.append(full_path_test)
 
 # All remaining imports
+from rangeQuery import RangeQuery as RQ
+from similarityQuery import SimilarityQuery as SQ
 import testFunctions as tF
 import Trajectory as T
 import Node as N
@@ -38,6 +40,30 @@ def testMultiPlot():
     traj_list = [traj1, traj2]
     Plot.plotTrajectories(traj_list)
 
-def testPlotting():
+def testRangeQueryPlot(trajectories, rtree):
+    # # Define the trajectories
+    # p1 = N.Node(1, 2.0, 3.0, 1.0)
+    # p2 = N.Node(2, 3.5, 6.0, 2.0)
+    # p3 = N.Node(3, 5.0, 10.0, 2.0)
+    # p4 = N.Node(4, 12.0, 13.0, 4.0)
+    # p5 = N.Node(5, 15.0, 20.0, 4.0)
+    # traj1 = T.Trajectory(1, [p1, p2, p3])
+    # traj2 = T.Trajectory(2, [p1, p3])
+    # traj3 = T.Trajectory(3, [p4, p5])
+    # traj_list = [traj1, traj2, traj3]
+
+    # Define the query
+    query = RQ({"x1":-730000.0, "x2":-727000.0, "y1":4548000.0, "y2":4557000.0, "t1":-1000000000000000000.0, "t2":1500000000000000000.0})
+    
+    # Run the function to be tested
+    Plot.plotRangeQuery(trajectories, query, rtree)
+
+def testSimilarityQueryPlot(trajectories, rtree):
+    query = SQ({"origin":trajectories[0], "t1":-1000000000000000000.0, "t2":1500000000000000000.0, "delta":1.0})
+    Plot.plotSimilarity(trajectories, query, rtree)
+
+def testPlotting(trajectories, rtree):
     testSinglePlot()
     testMultiPlot()
+    testRangeQueryPlot(trajectories, rtree)
+    # testSimilarityQueryPlot(trajectories, rtree)

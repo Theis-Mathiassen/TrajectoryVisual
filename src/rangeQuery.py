9222c1a9e414b354f9e80184f5150d0b9c258df4
MikkelBjørn <mikkel.bjoern@hotmail.com>
2025-03-10 11:59:55 +0100

    feat: added shared_equally

--- src/rangeQuery.py
+++ src/rangeQuery.py
@@ -12,6 +12,7 @@ class RangeQuery(Query):
     y2: float
     t1: float
     t2: float
+    flag: int
     
     def __init__(self, params):
         self.x1 = params["x1"]
@@ -20,6 +21,7 @@ class RangeQuery(Query):
         self.x2 = params["x2"]
         self.y2 = params["y2"]
         self.t2 = params["t2"]
+        self.flag = params["flag"]
 
     def run(self, rtree):
         # Gets nodes in range query
@@ -47,20 +49,28 @@ class RangeQuery(Query):
     
     def distribute(self, trajectories, matches):
         '''Function takes list of trajectories that are stored and distributes points to nodes in each respective trajectory that also appears in "matches".
-        matches is an object containing trajectory id, node id, and bounding box for all nodes intersecting the query. 
-        The function will distribute one point to a node in a trajectory based on which is closest to the center of the bounding box for the query.'''
-       
+        matches is an object containing trajectory id, node id, and bounding box for all nodes intersecting the query.'''
+        match self.flag :
+            case 1 :
+                return self.winner_takes_all(self, trajectories, matches)
+            case 2 : 
+                return self.shared_equally(self, trajectories, matches)
+
+
+
+    def winner_takes_all(self, trajectories, matches):
+        '''Give 1 point to the node closest to the origin for the range query for each trajectory'''
         def give_point(trajectory: Trajectory, node_id) :
             for n in trajectory.nodes :
                 if n.id == node_id :
                     n.score += 1
-        
+
         # TODO: Get center points from query
         centerx = 0
         centery = 0
         centert = 0
         q_bbox = [centerx, centery, centert]
-        
+
         # Key = Trajectory id, value = (Node id, distance)
         point_dict = dict()
 
@@ -83,3 +93,29 @@ class RangeQuery(Query):
             for t in trajectories :
                 if t.id == key :
                     give_point(t, value)
+
+    def shared_equally(self, trajectories, matches) : 
+        '''Give 1/m points to all m nodes in the trajectory that appeared in the range query'''
+        def give_point(trajectory: Trajectory, node_ids) :
+            for n in trajectory.nodes :
+                for m in node_ids : 
+                    if n.id == m :
+                        n.score += 1/(len(node_ids))
+        # Key = Trajectory id, value = (Node id list)
+        point_dict = dict()
+
+        # Get matches into correct format
+        matches = [(n.object, n.bbox) for n in matches]
+
+        # Put all nodes belonging to each trajectory together in a dict
+        for obj in matches : 
+            # If we see trajectory id for the first time, make it a key pointing to empty list in dict before adding node id to said list
+            if obj[0] not in point_dict :
+                point_dict[obj[0]] = []
+            point_dict[obj[0]].append(obj[1])
+
+        # Loop through trajectories and check if it appears in the dictionary
+        for t in trajectories : 
+            if t in point_dict : 
+                give_point(t, point_dict[t])
+

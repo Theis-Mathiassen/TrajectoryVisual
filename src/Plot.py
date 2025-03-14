53f5413ca82ccd1f9ae612747b4a1b9e8ea82212
HjalteJohnsen <101054167+HjalteJohnsen@users.noreply.github.com>
2025-03-14 15:31:36 +0100

    refactor: Added helper function for neater plotting code.

--- src/Plot.py
+++ src/Plot.py
@@ -8,31 +8,27 @@ from rangeQuery import RangeQuery as RQ
 from similarityQuery import SimilarityQuery as SQ
 from rtree import Index
 
+def trajectoryPlotting(traj: T):
+        xs = []
+        ys = []
+
+        for elem in traj.nodes:
+            xs.append(elem.x)
+            ys.append(elem.y)
+        
+        plt.plot(xs, ys)
+
 def plotTrajectory(traj: T):
     # Plots a single trajectory.
     # Input is assumed to be a 'Trajectory' instance.
-    xs = []
-    ys = []
-
-    for elem in traj.nodes:
-        xs.append(elem.x)
-        ys.append(elem.y)
-
-    plt.plot(xs, ys)
+    trajectoryPlotting(traj)
     plt.show()
 
 def plotTrajectories(traj: list[T]):
     # Plots multiple trajectories.
     # Input is assumed to be a list of 'Trajectory' instances.
     for Trajectory in traj:
-        xs = []
-        ys = []
-
-        for elem in Trajectory.nodes:
-            xs.append(elem.x)
-            ys.append(elem.y)
-
-        plt.plot(xs, ys)
+        trajectoryPlotting(Trajectory)
     plt.show()
 
 def plotRangeQuery(trajectories: list[T], query: RQ, rtree: Index):

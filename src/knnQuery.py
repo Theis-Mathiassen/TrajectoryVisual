4b4620a4051bb1aa27b4974e278c158e24536cb3
Grace Melchiors <melchiorsgrace@gmail.com>
2025-04-07 12:06:35 +0200

    temp | heuristic function
    Made a heuristic function that filters which trajectories to compute DTW for.
    
    Still errors that need to be fixed

--- src/knnQuery.py
+++ src/knnQuery.py
@@ -81,6 +81,12 @@ class KnnQuery(Query):
         return [x for x in trajectories_output if x.id in topKIds]
 
     def run2(self, rtree):
+
+        import time
+
+        previous = time.time()
+        print("\n --- Running KNN Query ---")
+
         hits = list(rtree.intersection((self.x1, self.y1, self.t1, self.x2, self.y2, self.t2), objects="raw"))
         
         hits = [(trajectory_id, node_id) for (trajectory_id, node_id) in hits if trajectory_id != self.trajectory.id]
@@ -105,22 +111,103 @@ class KnnQuery(Query):
         
         listOfTrajectorySegments = trajectories.items()
 
+        now = time.time()
+        print("Time to get trajectories: ", now - previous)
+        previous = now
+
         # Use DTW distance to compute similarity
         similarityMeasures = {}
+
         
         # Must be of type trajectory to be accepted
         originSegmentTrajectory = self.trajectory
+
         for segment in listOfTrajectorySegments:
             segmentTrajectory = Trajectory(segment[0], segment[1])
             similarityMeasures[segment[0]] = DTWDistance(originSegmentTrajectory, segmentTrajectory)
 
+        now = time.time()
+        print("Time to compute similarity no heuristic: ", now - previous)
+        previous = now
+
         # Sort by most similar, where the most similar have the smallest value
         similarityMeasures = sorted(similarityMeasures.items(), key=lambda x: x[1], reverse=False)
+
+
+        # get top k ids
+        topKIdsReal = [x[0] for x in similarityMeasures[:self.k]]
+
+        
+        # Use DTW distance to compute similarity
+        similarityMeasures = {}
+
+
+        # Estimate similarity with heuristic of runtime of T * N rather than T * N^2 as in DTW
+        sortedTrajectorySegments = []
+        originLength = len(originSegmentTrajectory.nodes)
+
+        for trajectory in listOfTrajectorySegments:
+            trajectoryLength = len(trajectory[1])
+            diff = 0
+            for i in range(min(originLength, trajectoryLength)):
+                diff += abs(originSegmentTrajectory.nodes.compressed()[i].x - trajectory[1].compressed()[i].x)
+                diff += abs(originSegmentTrajectory.nodes.compressed()[i].y - trajectory[1].compressed()[i].y)
+                diff += abs(originSegmentTrajectory.nodes.compressed()[i].t - trajectory[1].compressed()[i].t)
+            
+            # If the trajectories are of different lengths, add the difference in length to the similarity
+            differenceInLength = abs(originLength - trajectoryLength)
+            average = diff / min(originLength, trajectoryLength)
+            diff += (differenceInLength * average)
+
+            sortedTrajectorySegments.append((trajectory[0], trajectory[1], diff))
+
+        listOfTrajectorySegments = sorted(sortedTrajectorySegments, key=lambda x: x[2])
+        
+        # listOfTrajectorySegments = [(x[0], x[1]) for x in sortedTrajectorySegments]
+         
+        # Only look through the first k * 30 of the trajectories
+        length = len(listOfTrajectorySegments)
+        cutOff = min(self.k * 30, length - 1)
+
+        print("Length: ", length)
+
+        # If more are within similar distance, include them
+        valueForCut = listOfTrajectorySegments[cutOff][2] * 1.5
+        while listOfTrajectorySegments[cutOff][2] < valueForCut:
+            cutOff += 1
+            if cutOff >= length:
+                break 
+
+        print("Cut off: ", cutOff)
+
+        # Convert
+        listOfTrajectorySegments = [(x[0], x[1]) for x in listOfTrajectorySegments[:cutOff]]
+
+        now = time.time()
+        print("Time to get heuristic: ", now - previous)
+        previous = now
+
+
+        for segment in listOfTrajectorySegments:
+            segmentTrajectory = Trajectory(segment[0], segment[1])
+            similarityMeasures[segment[0]] = DTWDistance(originSegmentTrajectory, segmentTrajectory)
+
+        now = time.time()
+        print("Time to compute similarity: ", now - previous)
+        previous = now
+
+        # Sort by most similar, where the most similar have the smallest value
+        similarityMeasures = sorted(similarityMeasures.items(), key=lambda x: x[1], reverse=False)
+
 
         # get top k ids
         topKIds = [x[0] for x in similarityMeasures[:self.k]]
 
         trajectories_output = [Trajectory(trajectory_id, nodes) for trajectory_id, nodes in trajectories.items()]
+
+        now = time.time()
+        print("Top k ids before: ", topKIdsReal, "\nTop k ids after:  ", topKIds)
+        print("Time to sort: ", now - previous, "\n")
 
 
 

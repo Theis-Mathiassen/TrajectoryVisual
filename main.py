0746ec88a68678eecc5fbd0c57a7de13dbea963a
AndreasHolt <6665487+AndreasHolt@users.noreply.github.com>
2025-04-09 10:57:48 +0200

    chore: only run similarity queries now for debugging purposes

--- main.py
+++ main.py
@@ -58,18 +58,18 @@ def main(config):
     origRtreeParamsTraining : ParamUtil = ParamUtil(origRtree, origTrajectories, delta=10800) # Temporal window for T-Drive is 3 hours
     
 
-    origRtreeQueriesTraining.createRangeQueries(origRtree, origRtreeParamsTraining)
+    # origRtreeQueriesTraining.createRangeQueries(origRtree, origRtreeParamsTraining)
     origRtreeQueriesTraining.createSimilarityQueries(origRtree, origRtreeParamsTraining)
-    origRtreeQueriesTraining.createKNNQueries(origRtree, origRtreeParamsTraining)
+    # origRtreeQueriesTraining.createKNNQueries(origRtree, origRtreeParamsTraining)
     # origRtreeQueriesTraining.createClusterQueries(origRtree, origRtreeParamsTraining)
 
     # ---- Create evaluation queries -----
     origRtreeQueriesEvaluation : QueryWrapper = QueryWrapper(math.floor(config["numberOfEachQuery"] - config["numberOfEachQuery"] * config["trainTestSplit"]))
     origRtreeParamsEvaluation : ParamUtil = ParamUtil(origRtree, origTrajectories, delta=10800) # Temporal window for T-Drive is 3 hours
 
-    origRtreeQueriesEvaluation.createRangeQueries(origRtree, origRtreeParamsEvaluation)
+    # origRtreeQueriesEvaluation.createRangeQueries(origRtree, origRtreeParamsEvaluation)
     origRtreeQueriesEvaluation.createSimilarityQueries(origRtree, origRtreeParamsEvaluation)
-    origRtreeQueriesEvaluation.createKNNQueries(origRtree, origRtreeParamsEvaluation)
+    # origRtreeQueriesEvaluation.createKNNQueries(origRtree, origRtreeParamsEvaluation)
     # origRtreeQueriesEvaluation.createClusterQueries(origRtree, origRtreeParamsEvaluation)
 
 

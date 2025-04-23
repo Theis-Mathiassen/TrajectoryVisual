04c93fcf3e4ee62e6d963b92dd7b741273e9c095
Grace Melchiors <melchiorsgrace@gmail.com>
2025-04-23 11:25:03 +0200

    fix

--- src/clusterQuery.py
+++ src/clusterQuery.py
@@ -1,3 +1,21 @@
+import os
+import sys
+
+# Find the absolute path of the project directory
+absolute_path = os.path.dirname(__file__)
+
+# Define the relative paths of directories to import from
+relative_path_src = ".."
+relative_path_test = "../src"
+
+# Create the full path for these directories
+full_path_src = os.path.join(absolute_path, relative_path_src)
+full_path_test = os.path.join(absolute_path, relative_path_test)
+
+# Append them to the path variable of the system
+sys.path.append(full_path_src)
+sys.path.append(full_path_test)
+
 from src.Query import Query
 from src.Trajectory import Trajectory
 import numpy as np

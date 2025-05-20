Fix : Changed sigma for gaussian to me standard deviation instead of fixed. Made knn prettier. Changed gaussian params such that they are within rtree.bounds

src/QueryWrapper.py
@@ -22,6 +22,7 @@ class QueryWrapper:
         self.avgCoordinateValues = avgCoordinateValues
         self.rtree = rtree
         self.sigma = sigma
+        self.bounds = rtree.bounds
         
     def createRangeQueries(self, rtree, paramUtil : ParamUtil, flag: int = 1):
         if self.random:
@@ -32,6 +33,9 @@ class QueryWrapper:
         elif self.useGaussian:
             for query in range(self.numberOfEachQuery):
                 randomPoint = (np.random.normal(self.avgCoordinateValues, self.sigma, size=(1, 3)))[0]
+                randomPoint[0] = np.clip(randomPoint[0], self.bounds[0], self.bounds[3])
+                randomPoint[1] = np.clip(randomPoint[1], self.bounds[1], self.bounds[4])
+                randomPoint[2] = np.clip(randomPoint[2], self.bounds[2], self.bounds[5])
                 params = paramUtil.gaussianRangeParams(randomPoint)
                 params["flag"] = flag
                 self.RangeQueries.append(RangeQuery(params))
@@ -86,9 +90,9 @@ class QueryWrapper:
         return [*self.RangeQueries, *self.SimilarityQueries, *self.KNNQueries, *self.ClusterQueries]
     
     def _getNearestNode(self, point):
-        xcord = point[0]
-        ycord = point[1]
-        t = point[2]
+        xcord = np.clip(point[0], self.bounds[0], self.bounds[3])
+        ycord = np.clip(point[1], self.bounds[1], self.bounds[4])
+        t = np.clip(point[2], self.bounds[2], self.bounds[5])
         nearestList = list(self.rtree.nearest((xcord, ycord, t, xcord, ycord, t), 1, objects="raw")) # If multiple nodes are equal distance they are all returned, despite only getting top 1
 
         matches = len(nearestList)

train.py
@@ -15,6 +15,7 @@ import math
 import argparse
 from tqdm import tqdm
 import pandas as pd 
+import numpy as np
 import logging
 import traceback # traceback for information on python stack traces
 
@@ -54,13 +55,16 @@ def main(config):
     avgCoordinatesValue = None
     if USE_GAUSSIAN:
         avgCoordinatesValue = getAverageNodeCoordinates(origTrajectories)
+        stdCoordinatesValue = getStandardDerivationNodeCoordinates(origTrajectories, avgCoordinatesValue)
+        print(avgCoordinatesValue)
+        print(stdCoordinatesValue)
 
 
 
     # ---- Create training queries -----
     logger.info('Creating training queries.')
 
-    origRtreeQueriesTraining : QueryWrapper = QueryWrapper(config["numberOfEachQuery"], random=False, trajectories=origTrajectories, useGaussian=USE_GAUSSIAN, avgCoordinateValues=avgCoordinatesValue, rtree=origRtree, sigma=500)
+    origRtreeQueriesTraining : QueryWrapper = QueryWrapper(config["numberOfEachQuery"], random=False, trajectories=origTrajectories, useGaussian=USE_GAUSSIAN, avgCoordinateValues=avgCoordinatesValue, rtree=origRtree, sigma=stdCoordinatesValue)
     origRtreeParamsTraining : ParamUtil = ParamUtil(origRtree, origTrajectories, delta=10800) # Temporal window for T-Drive is 3 hours
     
     # Create the specified query type based on command line argument
@@ -145,6 +149,38 @@ def getAverageNodeCoordinates(trajectories):
 
     return avgCoordinateValues
 
+
+def getStandardDerivationNodeCoordinates(trajectories, averages):
+    logger.info("Using Gaussian distribution for creating queries instead of uniform distribution.")
+
+    # Find average location
+    totalNodes = 0
+    stdx = 0
+    stdy = 0
+    stdt = 0
+    for traj in tqdm(trajectories.values(), desc="Finding squared difference of nodes with mean"):
+        nodes = traj.nodes.compressed()
+        nodesx = np.array([node.x for node in nodes])
+        nodesy = np.array([node.y for node in nodes])
+        nodest = np.array([node.t for node in nodes])
+        totalNodes += len(nodes)
+        stdx += np.sum((nodesx - averages[0]) ** 2)
+        stdy += np.sum((nodesy - averages[1]) ** 2)
+        stdt += np.sum((nodest - averages[2]) ** 2)
+        
+        """for node in nodes:
+            avgx += node.x
+            avgy += node.y
+            avgt += node.t"""
+
+    stdx /= totalNodes
+    stdy /= totalNodes
+    stdt /= totalNodes
+
+
+    stdCoordinateValues = np.sqrt([stdx, stdy, stdt])
+
+    return stdCoordinateValues
 
 if __name__ == "__main__":
     logger.info("---------------------------    Train.py Main Start    ---------------------------")